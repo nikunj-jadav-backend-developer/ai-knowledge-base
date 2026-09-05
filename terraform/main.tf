8d60c0003d6a60561930c84d4488a6ec8af53296
Nikunj  Jadav <dirensj@gmail.com>
2026-09-05 15:25:46 +0530

    chore: temporarily allow ssh from all ips

--- terraform/main.tf
+++ terraform/main.tf
@@ -81,7 +81,7 @@ resource "aws_security_group" "web" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${var.ssh_allowed_ip}/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # HTTP
